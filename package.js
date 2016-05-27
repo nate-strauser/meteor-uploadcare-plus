acbffe9d03634b421cb1360b305a702c3714bb40
Rafael Muñoz <rafael.munoz@deliveryhero.com>
2016-05-27 16:11:49 +0200

    Upgrade to version 2.9.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name:"natestrauser:uploadcare-plus",
   summary: "uploadcare script packaged for Meteor with helpers and utilities",
-  version: "2.8.0",
+  version: "2.9.0",
   git: "https://github.com/nate-strauser/meteor-uploadcare-plus.git"
 });
 
