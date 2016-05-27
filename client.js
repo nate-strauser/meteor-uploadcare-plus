60a0bc268cf7d087507025711a01db0ce38fe4d5
Rafael Muñoz <rafael.munoz@deliveryhero.com>
2016-05-27 16:11:07 +0200

    Lint corrections

--- client.js
+++ client.js
@@ -1,37 +1,40 @@
-loadUploadcare = function(key, callback){
-  //todo - shift arguements to support just callback
-  if(typeof uploadcare === "undefined"){
-    if(!key && Meteor.settings && Meteor.settings.public && Meteor.settings.public.uploadcare && Meteor.settings.public.uploadcare.publickey)
-        key = Meteor.settings.public.uploadcare.publickey;
+loadUploadcare = function(key, callback) {
+  // TODO: Shift arguments to support just callback
+  if (typeof uploadcare === "undefined") {
+    if (! key && Meteor.settings && Meteor.settings.public &&
+        Meteor.settings.public.uploadcare &&
+        Meteor.settings.public.uploadcare.publickey) {
+      key = Meteor.settings.public.uploadcare.publickey;
+    }
 
-    if(key){
-        window.UPLOADCARE_PUBLIC_KEY = key;
+    if (key) {
+      window.UPLOADCARE_PUBLIC_KEY = key;
 
-        // Functions to run after the script tag has loaded
-        var loadCallback = function() {
-          if (Object.prototype.toString.call(callback) === "[object Function]")
-            callback();
-        };
+      // Functions to run after the script tag has loaded
+      var loadCallback = function() {
+        if (Object.prototype.toString.call(callback) === "[object Function]") {
+          callback();
+        }
+      };
 
-        // If the script doesn't load
-        var errorCallback = function(error) {
-          if(typeof console !== "undefined") {
-            console.log(error);
-          }
-        };
+      // If the script doesn't load
+      var errorCallback = function(error) {
+        if (typeof console !== "undefined") {
+          console.log(error);
+        }
+      };
 
-        // Generate a script tag
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://ucarecdn.com/widget/2.8.0/uploadcare/uploadcare.full.min.js";
-        script.onload = loadCallback;
-        script.onerror = errorCallback;
+      // Generate a script tag
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = "https://ucarecdn.com/widget/2.8.0/uploadcare/uploadcare.full.min.js";
+      script.onload = loadCallback;
+      script.onerror = errorCallback;
 
-        // Load the script tag
-        document.getElementsByTagName('head')[0].appendChild(script);
-      
-    }else{
-      if(typeof console !== "undefined") {
+      // Load the script tag
+      document.getElementsByTagName('head')[0].appendChild(script);
+    } else {
+      if (typeof console !== "undefined") {
         console.log("uploadcare-plus - tried to load but key not supplied");
       }
     }
@@ -40,14 +43,18 @@ loadUploadcare = function(key, callback){
 
 UI.registerHelper("uploadcareUUIDToImageUrl", function(uuid, operations) {
   var url = "";
-  if(uuid){
+  if (uuid){
     var cdn = "https://ucarecdn.com/"
-    if(Meteor.settings && Meteor.settings.public && Meteor.settings.public.uploadcare && Meteor.settings.public.uploadcare.cdn)
+    if (Meteor.settings && Meteor.settings.public &&
+        Meteor.settings.public.uploadcare &&
+        Meteor.settings.public.uploadcare.cdn) {
       cdn = Meteor.settings.public.uploadcare.cdn;
+    }
 
     url = cdn + uuid + "/";
-    if(operations)
+    if (operations) {
       url += operations;
+    }
   }
   return url;
 });
